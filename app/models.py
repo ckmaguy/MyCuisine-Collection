66995b160effaaac47ae26aacf07f409323aa883
ckmaguy <maguycoul@gmail.com>
2023-12-09 15:07:30 +0100

    On branch Kadidia Your branch is up to date with 'origin/Kadidia'. Changes to be committed: modified:   app/models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,10 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'  
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)  
     password_hash = db.Column(db.String(128))
+    date_joined = db.Column(db.DateTime, default=db.func.current_timestamp())  
+    profile_picture = db.Column(db.String(256))  
 
     # Relationship to link user with their recipes
     recipes = db.relationship('Recipe', backref='user', lazy='dynamic')
